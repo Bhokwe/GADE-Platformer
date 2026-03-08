45d5496077969ff1da24b101834c3ddb90c05b86
Bhokwe <165327116+Bhokwe@users.noreply.github.com>
2026-03-08 12:25:12 +0200

    Camera, Movement and Jump complete
    Used Cinemachine for the camera. Still unsure about it because it is a bit too rapid and wonky for me but maybe you will test and have a different experience. The jump has not problems. The assets and colliders are causing the player to just fall into stuff, so we might need to revert to primitive shapes.
    
    Once the other issues with colliders are solved, I will add double jumps and a dash. Possibly look into fixing the camera once and for all.
    
    Then we can start the UI and dialogue work.

--- GADE_Platfromer/Assets/_Game/Scripts/Player/PlayerMovement.cs
+++ GADE_Platfromer/Assets/_Game/Scripts/Player/PlayerMovement.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
@@ -13,19 +15,32 @@ public class PlayerController : MonoBehaviour
 
     // the settings for the players movement mechanics
     [Header("Movement Settings")]
-    public float moveSpeed;
+    public float moveSpeed; //player movement speed
 
     [Header("Jump Settings")]
-    public float jumpForce; 
+    public float jumpForce; //how much force is applied when the player jumps
 
+    [Header("Ground Check Settings")]
+    public Transform groundCheck; // to see if the player is on the ground
+    public float groundDistance = 0.4f; //the radius of the sphere used to check for the ground
+    public LayerMask groundMask;  //the layer for the ground objects
+    public bool isGrounded; // this will be used to check if the player is on the ground before allowing them to jump
 
-    
+    [Header("Camera Ref")]
+    public Transform mainCam;
+
+    [Header("Rotation Settings")]
+    public float turnSmoothTime; //how fast the player rotates
+    float turnSmoothVelocity; // smoothen the rotation 
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        //getting the components 
         rb = GetComponent<Rigidbody>();
         playerInput = GetComponent<PlayerInput>();
+        //the move and jump actions found in the IA asset
         moveAction = playerInput.actions.FindAction("Move");
         jumpAction = playerInput.actions.FindAction("Jump");
 
@@ -34,9 +49,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // checks if the player is on the ground by checking for collisions with the ground layer
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask); 
         PlayerMovement();
 
-        if (jumpAction.WasPressedThisFrame())
+
+        //when jump is pressed and the player is grounded, the player wil jump
+        if (jumpAction.WasPressedThisFrame() && isGrounded)
         {
             Jump();
         }
@@ -44,9 +63,33 @@ public class PlayerController : MonoBehaviour
 
     void PlayerMovement()
     {
+        //reading the raw input
+        Vector2 inputDirection = moveAction.ReadValue<Vector2>();
+        //converting it to a 3D direction vector
+        Vector3 direction = new Vector3(inputDirection.x, 0, inputDirection.y).normalized;
 
-        Vector2 direction = moveAction.ReadValue<Vector2>();
+        //Vector2 direction = moveAction.ReadValue<Vector2>(); switch back to it if something breaks
         transform.position += new Vector3(direction.x, 0, direction.y) * moveSpeed * Time.deltaTime;
+
+        //rotate only when the player is inputting that direction
+        if (direction.magnitude>=0.1f)
+        {
+            //eyy this math bro but anyway, this calculates the anle the character needs to face
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + mainCam.eulerAngles.y;
+
+            //eases the current rotation smoothly towards the target angle
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            //applies the rotation to the player
+            transform.rotation = Quaternion.Euler(0, angle, 0);
+
+            Vector3 moveDirection = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
+
+
+            //changes forward to the new input direction
+            transform.position += moveDirection.normalized * moveSpeed * Time.deltaTime;
+
+
+        }
 
     }
 
